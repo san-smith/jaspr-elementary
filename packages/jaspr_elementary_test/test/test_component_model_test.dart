93201f14edc23b9fbf93e35086797dcf4d1d19af
san-smith <san-smith@mail.ru>
2026-07-21 12:38:48 +0700

    chore: update jaspr_elementary_test dependencies and fix test

--- packages/jaspr_elementary_test/test/test_component_model_test.dart
+++ packages/jaspr_elementary_test/test/test_component_model_test.dart
@@ -3,10 +3,61 @@ import 'package:jaspr_elementary/jaspr_elementary.dart';
 import 'package:jaspr_elementary_test/jaspr_elementary_test.dart';
 import 'package:test/test.dart';
 
+void main() {
+  testComponentModel<_Cm, _Comp, _Model>(
+    'init runs model setup then didChangeDependencies',
+    () => _Cm(_Model()),
+    (cm, tester, context) async {
+      expect(context, isNotNull);
+
+      tester.init(initComponent: _Comp());
+
+      expect(cm.log, ['initComponentModel', 'didChangeDependencies']);
+      expect(cm.model, isNotNull);
+
+      tester.unmount();
+      expect(cm.log.last, 'dispose');
+    },
+  );
+
+  testComponentModel<_Cm, _Comp, _Model>(
+    'update calls didUpdateComponent',
+    () => _Cm(_Model()),
+    (cm, tester, context) async {
+      final first = _Comp();
+      final second = _Comp2();
+
+      tester.init(initComponent: first);
+      cm.log.clear();
+
+      tester.update(second);
+
+      expect(cm.log, ['didUpdateComponent']);
+      expect(identical(cm.component, second), isTrue);
+
+      tester.unmount();
+    },
+  );
+
+  testComponentModel<_Cm, _Comp, _Model>(
+    'handleError forwards to onErrorHandle',
+    () => _Cm(_Model()),
+    (cm, tester, context) async {
+      tester.init(initComponent: _Comp());
+
+      tester.handleError('e1');
+      expect(cm.lastError, 'e1');
+
+      tester.unmount();
+    },
+  );
+}
+
 class _Model extends ElementaryModel {}
 
 class _Comp extends ElementaryComponent<_Cm> {
-  _Comp() : super((_) => throw StateError('cmFactory is not used in this test'));
+  _Comp()
+    : super((_) => throw StateError('cmFactory is not used in this test'));
 
   @override
   Component build(_Cm cm) => Component.text('');
@@ -50,56 +101,3 @@ class _Cm extends ComponentModel<_Comp, _Model> {
 }
 
 class _Comp2 extends _Comp {}
-
-void main() {
-  testComponentModel<_Cm, _Comp, _Model>(
-    'init runs model setup then didChangeDependencies',
-    () => _Cm(_Model()),
-    (cm, tester, context) async {
-      expect(context, isNotNull);
-
-      tester.init(initComponent: _Comp());
-
-      expect(cm.log, [
-        'initComponentModel',
-        'didChangeDependencies',
-      ]);
-      expect(cm.model, isNotNull);
-
-      tester.unmount();
-      expect(cm.log.last, 'dispose');
-    },
-  );
-
-  testComponentModel<_Cm, _Comp, _Model>(
-    'update calls didUpdateComponent',
-    () => _Cm(_Model()),
-    (cm, tester, context) async {
-      final first = _Comp();
-      final second = _Comp2();
-
-      tester.init(initComponent: first);
-      cm.log.clear();
-
-      tester.update(second);
-
-      expect(cm.log, ['didUpdateComponent']);
-      expect(identical(cm.component, second), isTrue);
-
-      tester.unmount();
-    },
-  );
-
-  testComponentModel<_Cm, _Comp, _Model>(
-    'handleError forwards to onErrorHandle',
-    () => _Cm(_Model()),
-    (cm, tester, context) async {
-      tester.init(initComponent: _Comp());
-
-      tester.handleError('e1');
-      expect(cm.lastError, 'e1');
-
-      tester.unmount();
-    },
-  );
-}
